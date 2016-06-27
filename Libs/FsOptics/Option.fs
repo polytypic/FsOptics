5794f6e0c6e6adaa098b95f7c4188da10c78c8f3
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-06-27 09:03:59 +0300

    Option value traversal.

--- Libs/FsOptics/Option.fs
+++ Libs/FsOptics/Option.fs
@@ -1,5 +1,7 @@
 namespace FsOptics
 
 module Option =
+  let valueT U = function Some x -> U x </> Some
+                        | None   -> result None
   let valueL U = function Some v -> some U Some v
                         | None   -> none U Some
--- Libs/FsOptics/Option.fsi
+++ Libs/FsOptics/Option.fsi
@@ -1,4 +1,5 @@
 namespace FsOptics
 
 module Option =
+  val valueT: Optic<option<'a>, 'a, 'b, option<'b>>
   val valueL: Optic<option<'a>, option<'a>, 'b, option<'b>>
